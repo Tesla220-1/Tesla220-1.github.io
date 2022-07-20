1ab77dfeb3f93b06da0d438f055b9a31cae58da0
Vladislav <vladislav5008zo@gmail.com>
2022-07-20 19:36:15 +0300

    Общие доработки по сайту

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+	body = document.body;
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
+		body.classList.toggle('overflow-hidden');
     });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
+	if (document.documentElement.clientWidth < 800) {
+		menuItem.forEach(item => {
+			item.addEventListener('click', () => {
+				hamburger.classList.toggle('hamburger_active');
+				menu.classList.toggle('menu_active');
+				body.classList.toggle('overflow-hidden');
+			})
+		})
+	}
+
+    // flowing scroll
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        
+        const blockID = anchor.getAttribute('href').substr(1)
+        
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
         })
-    })
+      })
+    }
+    // flowing scroll end
 })
