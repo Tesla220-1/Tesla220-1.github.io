8eca933d1d2c41d34a85fee33243f9cb479e1df1
Владислав Речнов <v.rechnov@cinet.ru>
2022-07-24 17:07:38 +0300

    Правки в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
@@ -74,7 +73,6 @@ gulp.task('icons', function () {
 
 gulp.task('images', function () {
     return gulp.src("src/img/**/*")
-        .pipe(imagemin())
         .pipe(gulp.dest("build/img"))
         .pipe(browserSync.stream());
 });
